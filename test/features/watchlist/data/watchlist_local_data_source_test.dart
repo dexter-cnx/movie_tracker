cae1477622dc1eb61174dfbba7d807bedcd0cd83
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-07-24 07:03:24 +0700

    test: cover Hive watchlist data source behavior

---
+++ test/features/watchlist/data/watchlist_local_data_source_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:popcorn_movie_tracker/features/watchlist/data/watchlist_local_data_source.dart';
+import 'package:popcorn_movie_tracker/features/watchlist/domain/watchlist_item.dart';
+
+class MockBox extends Mock implements Box<Map> {}
+
+void main() {
+  late MockBox box;
+  late WatchlistLocalDataSource source;
+
+  setUp(() {
+    box = MockBox();
+    source = WatchlistLocalDataSource(box);
+  });
+
+  WatchlistItem item(String id, DateTime addedAt) => WatchlistItem(
+        id: id,
+        movieId: id.hashCode,
+        title: id,
+        status: WatchStatus.wantToWatch,
+        addedAt: addedAt,
+      );
+
+  test('getAll maps values and sorts newest first', () {
+    final older = item('older', DateTime.utc(2026, 1, 1));
+    final newer = item('newer', DateTime.utc(2026, 2, 1));
+    when(() => box.values).thenReturn([older.toMap(), newer.toMap()]);
+
+    final result = source.getAll();
+
+    expect(result.map((e) => e.id), ['newer', 'older']);
+  });
+
+  test('save persists item map under item id', () async {
+    final value = item('movie-1', DateTime.utc(2026, 1, 1));
+    when(() => box.put(any(), any())).thenAnswer((_) async {});
+
+    await source.save(value);
+
+    verify(() => box.put('movie-1', value.toMap())).called(1);
+  });
+
+  test('delete removes item by id', () async {
+    when(() => box.delete(any())).thenAnswer((_) async {});
+
+    await source.delete('movie-1');
+
+    verify(() => box.delete('movie-1')).called(1);
+  });
+}
